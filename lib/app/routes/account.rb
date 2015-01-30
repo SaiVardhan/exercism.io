3870479f8eba071d3a1bb23a829def01b88338a7
Sai Vardhan <saimca03@gmail.com>
2015-01-30 14:45:42 +0530

    added test case for updating source type

--- lib/app/routes/account.rb
+++ lib/app/routes/account.rb
@@ -13,9 +13,10 @@ module ExercismWeb
           halt 403, "You must be logged in to edit your email settings"
         end
 
-        current_user.email = params[:email]
+        current_user.email = params[:email] if params[:email]
+        current_user.source_type = params[:source_type] if params[:source_type]
         current_user.save
-        flash[:success] = 'Updated email address.'
+        flash[:success] = 'Updated Account Preferences.'
         redirect "/account"
       end
     end
--- test/acceptance/account_test.rb
+++ test/acceptance/account_test.rb
@@ -9,7 +9,7 @@ class AccountTest < AcceptanceTestCase
   def test_account_page_exists
     with_login(@user) do
       click_on 'Account'
-     # binding.pry  
+
       assert_css 'h1', text: 'Account'
       assert_content 'Exercises'
     end
@@ -23,21 +23,19 @@ class AccountTest < AcceptanceTestCase
       
       click_on 'Update'
       
-      assert_content 'Updated email address.'
+      assert_content 'Updated Account Preferences.'
       assert_equal 'some@email.com', find('[name=email]').value
     end
   end
 
   def test_creating_a_team
-    create_user(username: 'one_username', github_id: 12345,source_type: "DB")
-    create_user(username: 'two_username', github_id: 4567,source_type: "GIT")
+    create_user(username: 'one_username', github_id: 12345)
+    create_user(username: 'two_username', github_id: 4567)
 
     with_login(@user) do
       click_on 'Account'
-      require 'pry'
-     #  binding.pry
       click_on 'new team'
-      
+
       fill_in 'Slug', with: 'gocowboys'
       fill_in 'Name', with: 'Go Cowboys'
       fill_in 'Usernames', with: 'one_username, two_username'
@@ -49,5 +47,19 @@ class AccountTest < AcceptanceTestCase
       assert_content 'one_username'
       assert_content 'two_username'
     end
+  end
+  
+  
+  def test_changing_source_type
+    with_login(@user) do
+      click_on('Account')
+      require 'pry'
+      binding.pry
+      select('GIT', :from => 'source_type')
+      click_on 'Update'
+      
+      assert_content 'Updated Account Preferences.'
+      assert_equal 'GIT', find('[name=source_type]').value
+    end  
   end
 end
